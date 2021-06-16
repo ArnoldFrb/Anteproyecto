c0f9c1614dd70bd4a4fa260b02681247b41f9d66
ArnoldFrb <arnoldfrb@gmail.com>
2021-06-16 13:13:14 -0500

    Update UT_ConvocatoriaService.cs

--- Anteproyecto.Aplication.Test/UT_ConvocatoriaService.cs
+++ Anteproyecto.Aplication.Test/UT_ConvocatoriaService.cs
@@ -41,7 +41,7 @@ namespace Anteproyecto.Aplication.Test
             _dbContext.SaveChanges(); 
 
              //Act
-            var _convocatoria = new ConvocatoriaRequest {Id = 1, FechaInicio= new DateTime(2021,1,1) , FechaCierre = new DateTime(2021, 3, 1) };
+            var _convocatoria = new ConvocatoriaRequest {Id = 2, FechaInicio= new DateTime(2021,1,1) , FechaCierre = new DateTime(2021, 3, 1) };
             var response = _convocatoriaService.ActivarCargaProyectos(_convocatoria);
 
             //Assert
